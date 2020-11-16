daa6ecb66c3b8b7a0bdb6d54fe3f070481015bac
Sunny Jha <sunnyjha11897@gmail.com>
2020-11-16 21:46:26 +0530

    [SUNNY]ADD.Usecase2 performing crud operations using blue prism

---
+++ Address Book Files/SQLQuery3.sql
@@ -0,0 +1,7 @@
+--Insert data
+INSERT INTO addressbook (firstname,lastname,address,city,state,zip,phoneno,email)
+    VALUES('Vladimir','Putin','3/40, Broadway Street','Moscow','Narnia',989001,'2354618524','v.putin@gmail.com'),
+        ('Sherlock','Holmes','Baker Street','London','England',567897,'7654874','sherlocked@gmail.com');
+
+SELECT * FROM addressbook;
+
